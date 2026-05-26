64b45b6e4db997b02681a5fe531454b1a5fb6f9c
splinterkuo <71313803+splinterkuo@users.noreply.github.com>
2026-05-26 12:14:48 +0200

    switch to pyinstaller

--- setup.py
+++ setup.py
@@ -1,4 +1,20 @@
+from setuptools import setup
+
+APP = ['Applecode.py']
+DATA_FILES = []
 OPTIONS = {
     'argv_emulation': False,
-    'packages': ['selenium', 'webdriver_manager', 'tkinter'],
+    'packages': [
+        'selenium',
+        'webdriver_manager',
+        'tkinter',
+        'requests',
+    ],
 }
+
+setup(
+    app=APP,
+    data_files=DATA_FILES,
+    options={'py2app': OPTIONS},
+    setup_requires=['py2app'],
+)
